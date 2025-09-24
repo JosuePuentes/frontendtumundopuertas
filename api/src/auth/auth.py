3a445e1b732a4540a3b9503cd2d7bf2cb55bdc08
JosuePuentes <puentesanubis1@gmail.com>
2025-09-24 15:08:39 -0400

    feat: Implement admin role check for user management and add auth dependencies

--- api/src/auth/auth.py
+++ api/src/auth/auth.py
@@ -2,9 +2,12 @@ from passlib.context import CryptContext
 from ..config.config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
 from datetime import datetime, timedelta
 import jwt
-
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
@@ -22,3 +25,23 @@ def create_admin_access_token(admin: dict, expires_delta: timedelta = timedelta(
     }
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: str = payload.get("id")
+        if user_id is None:
+            raise credentials_exception
+        return payload
+    except jwt.PyJWTError:
+        raise credentials_exception
+
+async def get_current_admin_user(current_user: dict = Depends(get_current_user)):
+    if current_user.get("rol") != "admin":
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not enough permissions")
+    return current_user
