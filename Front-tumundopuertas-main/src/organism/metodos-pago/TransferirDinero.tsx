35d9c570eb42362c99c1809c27ac7405017e0d56
JosuePuentes <puentesanubis1@gmail.com>
2025-10-08 00:14:02 -0400

    fix(a11y): add descriptions to dialogs

--- Front-tumundopuertas-main/src/organism/metodos-pago/TransferirDinero.tsx
+++ Front-tumundopuertas-main/src/organism/metodos-pago/TransferirDinero.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Button } from "../../components/ui/button";
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../../components/ui/dialog";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription } from "../../components/ui/dialog";
 import { Input } from "../../components/ui/input";
 import { Label } from "../../components/ui/label";
 import { transferirDinero } from "../../lib/api";
