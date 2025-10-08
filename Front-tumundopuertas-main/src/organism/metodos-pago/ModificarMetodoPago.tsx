534f71d1dfc1e88f9bb604b54ff104c1fededa90
JosuePuentes <puentesanubis1@gmail.com>
2025-10-08 13:38:45 -0400

    fix(build): Remove unused DialogDescription imports

--- Front-tumundopuertas-main/src/organism/metodos-pago/ModificarMetodoPago.tsx
+++ Front-tumundopuertas-main/src/organism/metodos-pago/ModificarMetodoPago.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Button } from "../../components/ui/button";
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription } from "../../components/ui/dialog";
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../../components/ui/dialog";
 import { Input } from "../../components/ui/input";
 import { Label } from "../../components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../../components/ui/select";
