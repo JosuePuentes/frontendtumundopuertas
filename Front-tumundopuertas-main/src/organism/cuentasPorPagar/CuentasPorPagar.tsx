a88d9120e17c879ea177449189cf717a9764184d
JosuePuentes <puentesanubis1@gmail.com>
2025-10-31 21:47:05 -0400

    Aumentar ancho del modal a 1400px y reducir altura a 700px para mejor visualización de items sin scroll horizontal

--- Front-tumundopuertas-main/src/organism/cuentasPorPagar/CuentasPorPagar.tsx
+++ Front-tumundopuertas-main/src/organism/cuentasPorPagar/CuentasPorPagar.tsx
@@ -616,7 +616,7 @@ const CuentasPorPagar: React.FC = () => {
 
       {/* Modal Crear Cuenta */}
       <Dialog open={modalCrearOpen} onOpenChange={setModalCrearOpen}>
-        <DialogContent className="max-w-[1024px] w-[1024px] max-h-[768px] h-[768px] overflow-hidden flex flex-col">
+        <DialogContent className="max-w-[1400px] w-[1400px] max-h-[700px] h-[700px] overflow-hidden flex flex-col">
           <DialogHeader className="flex-shrink-0">
             <DialogTitle className="text-2xl">Agregar Cuenta por Pagar</DialogTitle>
             <DialogDescription>
