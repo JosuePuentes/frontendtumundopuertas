e195cbe6fb072ea0c6125ade36bf756a1c595b4b
JosuePuentes <puentesanubis1@gmail.com>
2025-11-05 14:58:35 -0400

    Fix: Agregar paréntesis de cierre faltante en operador ternario - Corregido cierre del operador ternario en sección Más Movidos que causaba error de compilación

--- Front-tumundopuertas-main/src/organism/panelControlLogistico/PanelControlLogistico.tsx
+++ Front-tumundopuertas-main/src/organism/panelControlLogistico/PanelControlLogistico.tsx
@@ -1008,6 +1008,7 @@ const PanelControlLogistico: React.FC = () => {
                       </tbody>
                     </table>
                   </div>
+                  )}
                 </CardContent>
               </Card>
             </TabsContent>
